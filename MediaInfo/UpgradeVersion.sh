e320b6dfbc8f0a0ff6a0161633ec8b999a5310f1
Almin <almin@almin.tf>
2016-01-26 01:27:36 +0100

    Support of MSVC2015 + MC-server
    Signed-off-by: Almin <almin@almin.tf>

--- MediaInfo/UpgradeVersion.sh
+++ MediaInfo/UpgradeVersion.sh
@@ -13,7 +13,7 @@ function btask.UpgradeVersion.run () {
     if [ $(b.opt.get_opt --repo) ]; then
         Repo=$(sanitize_arg $(b.opt.get_opt --repo))
     else
-        Repo="https://github.com/MediaArea/MediaInfo.git"
+        Repo="https://github.com/MediaArea/MediaInfo"
     fi
 
     if [ $(b.opt.get_opt --source-path) ]; then
@@ -21,7 +21,6 @@ function btask.UpgradeVersion.run () {
     else
         getRepo $Repo "$WDir"
         MI_source="$WDir"/MediaInfo
-        # For lisibility after git
         echo
     fi
 
@@ -31,14 +30,22 @@ function btask.UpgradeVersion.run () {
     MI_files[((index++))]="Source/GUI/Cocoa/MediaInfo.xcodeproj/project.pbxproj"
     MI_files[((index++))]="Project/GNU/CLI/configure.ac"
     MI_files[((index++))]="Project/GNU/GUI/configure.ac"
+    MI_files[((index++))]="Project/GNU/mediainfo.spec"
+    MI_files[((index++))]="Project/GNU/mediainfo.dsc"
+    MI_files[((index++))]="debian/control"
     MI_files[((index++))]="debian/changelog"
+    MI_files[((index++))]="Project/OBS/deb9.dsc"
+    MI_files[((index++))]="Project/OBS/deb9.debian/control"
     MI_files[((index++))]="Project/OBS/deb9.debian/changelog"
+    MI_files[((index++))]="Project/OBS/deb7.dsc"
+    MI_files[((index++))]="Project/OBS/deb7.debian/control"
     MI_files[((index++))]="Project/OBS/deb7.debian/changelog"
+    MI_files[((index++))]="Project/OBS/deb6.dsc"
+    MI_files[((index++))]="Project/OBS/deb6.debian/control"
     MI_files[((index++))]="Project/OBS/deb6.debian/changelog"
     MI_files[((index++))]="Project/OBS/obs_mediainfo"
     MI_files[((index++))]="Project/Solaris/mkpkg"
 
-    # Replace old version by new version
     for MI_file in ${MI_files[@]}
     do
         echo "${MI_source}/${MI_file}"
@@ -46,47 +53,17 @@ function btask.UpgradeVersion.run () {
     done
 
     echo
-    echo "Update ${MI_source}/Project/GNU/mediainfo.spec"
-    updateFile "%define mediainfo_version           $Version_old_escaped" "%define mediainfo_version           $Version_new" "${MI_source}"/Project/GNU/mediainfo.spec
-    #updateFile "* Tue Jan 01 2009 MediaArea.net SARL <info@mediaarea.net> - $Version_old_escaped" "* Tue Jan 01 2009 MediaArea.net SARL <info@mediaarea.net> - $Version_new" "${MI_source}"/Project/GNU/mediainfo.spec
-    echo
-
-    echo "Update ${MI_source}/Project/GNU/mediainfo.dsc"
-    updateFile "Version: $Version_old_escaped" "Version: $Version_new" "${MI_source}"/Project/GNU/mediainfo.dsc
-    # sed will take the last of the longuest strings first and
-    # will replace the 3 lines
-    updateFile "00000000000000000000000000000000 000000 mediainfo_$Version_old_escaped" "00000000000000000000000000000000 000000 mediainfo_$Version_new" "${MI_source}"/Project/GNU/mediainfo.dsc
-    
-    echo
-    echo "Update ${MI_source}/Project/OBS/deb9.dsc"
-    updateFile "Version: $Version_old_escaped" "Version: $Version_new" "${MI_source}"/Project/OBS/deb9.dsc
-    updateFile "00000000000000000000000000000000 000000 mediainfo_$Version_old_escaped" "00000000000000000000000000000000 000000 mediainfo_$Version_new" "${MI_source}"/Project/OBS/deb9.dsc
-
-    echo
-    echo "Update ${MI_source}/Project/OBS/deb7.dsc"
-    updateFile "Version: $Version_old_escaped" "Version: $Version_new" "${MI_source}"/Project/OBS/deb7.dsc
-    updateFile "00000000000000000000000000000000 000000 mediainfo_$Version_old_escaped" "00000000000000000000000000000000 000000 mediainfo_$Version_new" "${MI_source}"/Project/OBS/deb7.dsc
-
-    echo
-    echo "Update ${MI_source}/Project/OBS/deb6.dsc"
-    updateFile "Version: $Version_old_escaped" "Version: $Version_new" "${MI_source}"/Project/OBS/deb6.dsc
-    updateFile "00000000000000000000000000000000 000000 mediainfo_$Version_old_escaped" "00000000000000000000000000000000 000000 mediainfo_$Version_new" "${MI_source}"/Project/OBS/deb6.dsc
-
-    echo
     echo "Passage for major.minor.patch.build..."
     unset -v MI_files
     index=0
     MI_files[((index++))]="Project/BCB/GUI/MediaInfo_GUI.cbproj"
-    MI_files[((index++))]="Project/MSVC2008/GUI/MediaInfo_GUI.rc"
-    MI_files[((index++))]="Project/MSVC2008/CLI/MediaInfo_CLI.rc"
-    MI_files[((index++))]="Project/MSVC2010/GUI/MediaInfo_GUI.rc"
-    MI_files[((index++))]="Project/MSVC2010/CLI/MediaInfo_CLI.rc"
     MI_files[((index++))]="Project/MSVC2012/GUI/MediaInfo_GUI.rc"
     MI_files[((index++))]="Project/MSVC2012/CLI/MediaInfo_CLI.rc"
     MI_files[((index++))]="Project/MSVC2013/GUI/MediaInfo_GUI.rc"
     MI_files[((index++))]="Project/MSVC2013/CLI/MediaInfo_CLI.rc"
+    MI_files[((index++))]="Project/MSVC2015/GUI/MediaInfo_GUI.rc"
+    MI_files[((index++))]="Project/MSVC2015/CLI/MediaInfo_CLI.rc"
 
-    # Replace old version by new version
     for MI_file in ${MI_files[@]}
     do
 
