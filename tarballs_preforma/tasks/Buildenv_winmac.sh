fc14d8867e05462759fb0ae33e56b92b576685a7
Almin <almin@almin.tf>
2016-03-01 16:23:37 +0100

    Leaving the .bat for win but not for mac
    Signed-off-by: Almin <almin@almin.tf>

--- tarballs_preforma/tasks/Buildenv_winmac.sh
+++ tarballs_preforma/tasks/Buildenv_winmac.sh
@@ -16,7 +16,8 @@ function btask.Buildenv_winmac.run () {
 
     echo
     echo "Generate win/mac build environment..."
-    wget -nd -q -P tmp "https://mediaarea.net/download/source/mediaconch/${MC_version}/mediaconch_${MC_version}_AllInclusive.7z"
+    #wget -nd -q -P tmp "https://mediaarea.net/download/source/mediaconch/${MC_version}/mediaconch_${MC_version}_AllInclusive.7z"
+    wget -nd -q -P tmp "http://leprovost.info/fichiers/ma/releases/source/mediaconch/16.02/mediaconch_16.02_AllInclusive.7z"
     cd tmp
     7za x mediaconch_${MC_version}_AllInclusive.7z > /dev/null
 
@@ -24,14 +25,14 @@ function btask.Buildenv_winmac.run () {
     rm -fr mediaconch_AllInclusive/ZenLib
     rm -fr mediaconch_AllInclusive/MediaInfoLib
     rm -fr mediaconch_AllInclusive/MediaConch
-    rm -f mediaconch_AllInclusive/*bat
 
     echo "Create Windows package (buildenv01)..."
     mv mediaconch_AllInclusive buildenv01
     zip -q -r ../buildenv01-$Date.zip buildenv01
 
     echo "Create Mac package (buildenv05)..."
-    cp -r buildenv01 buildenv05
+    mv buildenv01 buildenv05
+    rm -f buildenv05/*bat
     zip -q -r ../buildenv05-$Date.zip buildenv05
 
     cd ..
