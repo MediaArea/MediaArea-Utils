c49121b34a1758a19e112be905d93c683b1aa278
Florent Tribouilloy <florent@mediaarea.net>
2016-06-21 16:21:42 +0200

    Tool/MIL/DIFF: add a tool to make diff between 2 versions
    Signed-off-by: Florent Tribouilloy <florent@mediaarea.net>

---
+++ tools/mil-diff/mil-diff.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+
+CURRENT_PATH=$(dirname "$0")
+
+NEW_REPO="https://github.com/MediaArea/MediaInfoLib"
+NEW_BRANCH="master"
+
+while getopts "r:b: " option; do
+    case "$option" in
+        r)
+            NEW_REPO="$OPTARG"
+            ;;
+        b)
+            NEW_BRANCH="$OPTARG"
+            ;;
+        \?)
+            exit 1;
+            ;;
+    esac
+done
+
+#old
+if [ ! -d "$CURRENT_PATH/master" ]
+then
+    git clone --depth 1 https://github.com/MediaArea/MediaInfoLib "$CURRENT_PATH/master"
+fi
+
+MASTER_COMPIL_PATH="$CURRENT_PATH/master/Project/GNU/Library/"
+cd "$MASTER_COMPIL_PATH"
+./autogen.sh
+./configure --with-libcurl
+make
+cd ../../../..
+
+#new
+if [ -d "$CURRENT_PATH/new" ]
+then
+    rm -rf "$CURRENT_PATH/new"
+fi
+
+git clone --depth 1 "$NEW_REPO" -b "$NEW_BRANCH" "$CURRENT_PATH/new"
+
+NEW_COMPIL_PATH="$CURRENT_PATH/new/Project/GNU/Library/"
+cd "$NEW_COMPIL_PATH"
+./autogen.sh
+./configure --with-libcurl
+make
+cd ../../../..
+
+
+PATH_FILES="$CURRENT_PATH/files"
+# clone sample test if needed
+if [ ! -d "$PATH_FILES" ]
+then
+    git clone --depth 1 https://github.com/MediaArea/MediaArea-RegressionTestingFiles "$PATH_FILES"
+fi
+
+cd files
+git fetch origin
+git rebase origin/master
+cd ..
+
+# create binary
+PKG_CONFIG_PATH_MASTER=`pwd`/master/Project/GNU/Library PKG_CONFIG_PATH_NEW=`pwd`/new/Project/GNU/Library make
+
+
+# create ouput directory
+mkdir -p tmp
+mkdir -p error
+rm -rf tmp/* error/*
+
+
+#test files
+unset args
+while IFS= read -r i; do
+    FILE_NAME=$(basename "$i")
+    OUTPUT_MASTER_NAME="$CURRENT_PATH/tmp/$FILE_NAME.master.xml"
+    `$CURRENT_PATH/create_xml_master "$PATH_FILES/$i" "$OUTPUT_MASTER_NAME"`
+    if test $? -ne 0
+    then
+        echo "Master MediaInfoLib cannot create XML" >&2;
+        continue;
+    fi
+
+    OUTPUT_NEW_NAME="$CURRENT_PATH/tmp/$FILE_NAME.new.xml"
+    `$CURRENT_PATH/create_xml_new "$PATH_FILES/$i" "$OUTPUT_NEW_NAME"`
+    if test $? -ne 0
+    then
+        echo "New MediaInfoLib cannot create XML" >&2;
+        continue;
+    fi
+
+    DIFF=`diff -u "$OUTPUT_MASTER_NAME" "$OUTPUT_NEW_NAME"`
+    if test $? -ne 0
+    then
+        echo "$PATH_FILES/$i differs" >&2;
+        ERROR_DIR="error/$i"
+        mkdir -p "$ERROR_DIR"
+        echo "$DIFF" > "$ERROR_DIR/$FILE_NAME.diff";
+        cp "$OUTPUT_MASTER_NAME" "$ERROR_DIR/."
+        cp "$OUTPUT_NEW_NAME" "$ERROR_DIR/."
+        continue;
+    fi
+done < "$PATH_FILES/files.txt"
