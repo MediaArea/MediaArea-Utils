3c34b46e5977a4910318281df5ccebd08b99899d
Maxime Gervais <gervais.maxime@gmail.com>
2016-10-17 00:28:54 +0200

    Preforma: create mac buildenv from precompiled deps
    + Add readmes
    
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

---
+++ tarballs_preforma/readmes/Readme_windows.txt
@@ -0,0 +1,22 @@
+----------
+MediaConch
+----------
+
+Website: https://mediaarea.net/MediaConch
+
+---------
+Licensing
+---------
+
+All open source digital assets for the software developed by MediaArea during the PREFORMA project will be made available under the open access license: Creative Commons license attribution – Sharealike 4.0 International (CC BY-SA v4.0). All assets will exist in open file formats within an open platform (an open standard as defined in the European Interoperability Framework for Pan-European eGovernment Service (version 1.0 2004)).
+
+-----------------------
+How to build MediaConch
+-----------------------
+Download and unzip the src01*.zip. Enter the new directory created.
+Download and unzip the buildenv01*.zip.
+
+Move the content of the buildenv01 directory to the current directory.
+
+Execute the .bat file corresponding to your Visual Studio version.
+In Visual Studio interface, select Release as target then build the solution.
---
+++ tarballs_preforma/tasks/Buildenv_mac.sh
@@ -0,0 +1,35 @@
+# MediaArea-Utils/tarballs_preforma/tasks/Buildenv_winmac.sh
+# Generate the tarballs asked by Preforma — Win/Mac build env.
+
+# Copyright (c) MediaArea.net SARL. All Rights Reserved.
+# Use of this source code is governed by a BSD-style license that
+# can be found in the License.html file in the root of the source
+# tree.
+
+function btask.Buildenv_mac.run () {
+    source $(dirname ${BASH_SOURCE[0]})/../../build_release/Config.sh
+
+    if b.path.dir? tmp; then
+        rm -fr tmp/*;
+    else
+        mkdir tmp
+    fi
+
+    echo
+    echo "Generate mac build environment..."
+    cd tmp
+    mkdir buildenv05
+    scp -q -r -P $Mac_SSH_port $Mac_SSH_user@$Mac_IP:$Mac_working_dir/releases/MediaConch_CLI_GNU_FromSource/{libevent,libxslt,jansson,libxml2,sqlite} buildenv05
+
+    # Add Qt binaries
+    scp -q -r -P $Mac_SSH_port $Mac_SSH_user@$Mac_IP:Qt buildenv05
+    sed -i 's/xcrun -find xcrun/xcrun -find xcodebuild/g' buildenv05/Qt/5.3/clang_64/mkspecs/features/mac/default_pre.prf
+    sed -i 's/macosx10.8/macosx/g' buildenv05/Qt/5.3/clang_64/mkspecs/qdevice.pri
+
+    echo "Create Mac package (buildenv05)..."
+    zip -q -r ../buildenv05-$Date.zip buildenv05
+
+    cd ..
+    rm -fr tmp
+
+}
