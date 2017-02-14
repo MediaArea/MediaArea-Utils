90cabc14bf1245fb2075fa2979386b4bcc024e50
Maxime Gervais <gervais.maxime@gmail.com>
2017-02-14 16:30:30 +0100

    Repo.py update
    + activation packages version number
    + add activation packages to repositories
    + fix RHEL/CentOS5
    + generic activations packages for all supported distributions
    
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- build_release/Repo.py
+++ build_release/Repo.py
@@ -14,7 +14,11 @@ import re
 # Sign_rpm_package - sign the given rpm with the configured key
 #
 def Sign_rpm_package(Package):
-    Command = "rpm --resign --define '_gpg_name %s' %s" % (Configuration["Repo_key"]["key"], Package)
+    Command = "rpm --addsign --define '%%_gpg_name %s' " % Configuration["Repo_key"]["key"]
+    Command = Command + "--define '__gpg_sign_cmd %{__gpg} gpg --batch --no-verbose --no-armor "
+    Command = Command + "--force-v3-sigs --digest-algo=sha1 --passphrase-fd 3  --no-secmem-warning "
+    Command = Command + "-u \"%s\" -sbo %%{__signature_filename} " % Configuration["Repo_key"]["key"]
+    Command = Command + "%%{__plaintext_filename}' %s" % Package
 
     Proc = pexpect.spawn(Command, env={"LC_ALL": "C"})
     Proc.expect("Enter pass phrase: ")
@@ -29,19 +33,23 @@ def Sign_rpm_package(Package):
 #
 # Create_repo_rpm - create repo activation package
 #
-def Create_repo_rpm(Path, Repo, Distribution, Release = False):
-    Repo_directory = os.path.relpath(Path, Configuration["Repo_path"])
+def Create_repo_rpm(Path, Repo, Release = False):
+    Version = Configuration["Repo_version"]
 
-    Repo_file = Configuration["Repo_file_template"].replace("REPO_NAME", Repo)
-    Repo_file = Repo_file.replace("REPO_URL", Configuration["Repo_url"])
-    Repo_file = Repo_file.replace("REPO_DIRECTORY", Repo_directory)
-    Repo_file = Repo_file.replace("DISTRIBUTION", Distribution.replace("_", " "))
+    Repo_url = os.path.join(Configuration["Repo_url"], "rpm", "releases" if Release else "snapshots")
 
-    Repo_spec = Configuration["Repo_spec_template"].replace("REPO_NAME", Repo)
-    Repo_spec = Repo_spec.replace("REPO_URL", Configuration["Repo_url"])
-    Repo_spec = Repo_spec.replace("REPO_DIRECTORY", Repo_directory)
-    Repo_spec = Repo_spec.replace("DISTRIBUTION", Distribution.replace("_", " "))
+    Repo_spec_template = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "repo_templates", "rpm_spec_template"), "r")
+    Repo_spec = Repo_spec_template.read()
+    Repo_spec_template.close()
+
+    Repo_spec = Repo_spec.replace("REPO_NAME", Repo)
+    Repo_spec = Repo_spec.replace("REPO_URL", Repo_url)
+    Repo_spec = Repo_spec.replace("PKG_VERSION", Version.split("-")[0])
+    Repo_spec = Repo_spec.replace("PKG_RELEASE", Version.split("-")[1])
     Repo_spec = Repo_spec.replace("DATE", datetime.date.today().strftime("%a %b %d %Y"))
+
+    Command = ["gpg", "--export", "--armor", "-u", Configuration["Repo_key"]["key"]]
+    Repo_spec = Repo_spec.replace("REPO_KEY", subprocess.check_output(Command))
 
     Build_root = os.path.join(os.getenv("HOME"), "rpmbuild")
 
@@ -50,58 +58,87 @@ def Create_repo_rpm(Path, Repo, Distribution, Release = False):
                 os.makedirs(os.path.join(Build_root, Directory))
 
     # Clean buildroot
-    Package_filename = os.path.join(Build_root, "RPMS", "noarch", "repo-%s-1.0-1.noarch.rpm" % Repo)
-    Repo_filename = os.path.join(Build_root, "SOURCES", "repo-%s.repo" % Repo)
+    Package_filename = os.path.join(Build_root, "RPMS", "noarch", "repo-%s-%s.noarch.rpm" % (Repo, Version))
     Spec_filename = os.path.join(Build_root, "SPECS", "repo-%s.spec" % Repo)
-    Key_filename = os.path.join(Build_root, "SOURCES", "GPG-KEY-%s" % Repo)
 
-    for File in [Package_filename, Repo_filename, Spec_filename, Key_filename]:
+    for File in [Package_filename, Spec_filename]:
         if os.path.exists(File):
             os.remove(File)
-
-    # Add repo file
-    Destination = open(Repo_filename, "w")
-    Destination.write(Repo_file)
-    Destination.close()
 
     # Add spec file
     Destination = open(Spec_filename, "w")
     Destination.write(Repo_spec)
     Destination.close()
 
-    # Add key
-    Destination = open(Key_filename, "w")
-    Command = ["gpg", "--export", "--armor", "-u", Configuration["Repo_key"]["key"]]
-    subprocess.call(Command, stdout=Destination)
-    Destination.close()
-
     # Make rpm package
-    Command = ["rpmbuild", "-bb", Spec_filename]
+    Command = ["rpmbuild", "-bb", Spec_filename,
+               "--define", "%_source_filedigest_algorithm 0",
+               "--define", "%_binary_filedigest_algorithm 0",
+               "--define", "%_source_payload w0.bzdio",
+               "--define", "%_binary_payload w0.bzdio"]
     subprocess.call(Command, stdout=OUT, stderr=OUT)
 
-    Package_destination = os.path.join(Path, "repo-%s-1.0-1.%s.noarch.rpm" % (Repo, Distribution))
+    Package_destination = os.path.join(Path, "repo-%s-%s.noarch.rpm" % (Repo, Version))
 
     if not os.path.exists(Package_filename):
-        print("ERROR: activation package failed for distribution %s" % Distribution)
+        print("ERROR: rpm activation package failed")
     else:
         shutil.move(Package_filename, Package_destination)
         Sign_rpm_package(Package_destination)
 
     # Clean
-    for File in [Package_filename, Repo_filename, Spec_filename, Key_filename]:
+    for File in [Package_filename, Spec_filename]:
         if os.path.exists(File):
             os.remove(File)
 
 #
 # Create_repo_deb - create repo activation package
-def Create_repo_deb(Path, Repo, Distribution, Release = False):
-    Control_file = Configuration["Repo_control_template"].replace("REPO_NAME", Repo)
-    Control_file = Control_file.replace("DISTRIBUTION", Distribution)
+#
+def Create_repo_deb(Path, Repo, Release = False):
+    Version = Configuration["Repo_version"]
 
-    List_file = "deb %s/deb/%s %s main" % (Configuration["Repo_url"], os.path.basename(Path), Distribution)
+    Control_file_template = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "repo_templates", "deb_control_template"), "r")
+    Control_file = Control_file_template.read()
+    Control_file_template.close()
+
+    Control_file = Control_file.replace("REPO_NAME", Repo)
+    Control_file = Control_file.replace("VERSION_FULL", Version)
+
+    Script_file_template = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "repo_templates", "deb_postinst_template"), "r")
+    Script_file = Script_file_template.read()
+    Script_file_template.close()
+
+    Script_file = Script_file.replace("REPO_NAME", Repo)
+
+    Debian_releases = ""
+    for Debian_version, Debian_codename in sorted(Configuration["Debian_names"].items()):
+        Debian_releases = Debian_releases + "," if Debian_releases else ""
+
+        if Debian_version == "Debian_Next_ga":
+            Debian_releases = Debian_releases + Configuration["Debian_testing"] + ":"
+        else:
+            Debian_releases = Debian_releases + re.split('_|\.', Debian_version)[-2] + ":"
+
+        Debian_releases = Debian_releases + Configuration["Debian_names"][Debian_version]
+
+    Ubuntu_releases = ""
+    for Ubuntu_version, Ubuntu_codename in sorted(Configuration["Ubuntu_names"].items()):
+        Ubuntu_releases = Ubuntu_releases + "," if Ubuntu_releases else ""
+
+        if Ubuntu_version == "Ubuntu_Next_standard":
+             Ubuntu_releases = Ubuntu_releases + Configuration["Ubuntu_testing"] + ":"
+        else:
+            Ubuntu_releases = Ubuntu_releases + Ubuntu_version.split('_')[-1].upper() + ":"
+
+        Ubuntu_releases = Ubuntu_releases + Configuration["Ubuntu_names"][Ubuntu_version]
+
+    Script_file = Script_file.replace("DEBIAN_RELEASES", Debian_releases)
+    Script_file = Script_file.replace("UBUNTU_RELEASES", Ubuntu_releases)
+
+    List_file = "deb %s/deb/DISTRIBUTION CODENAME%s main" % (Configuration["Repo_url"], "" if Release else "-snapshots")
 
     Build_root = "/tmp"
-    Build_dir = "repo-" + Repo + "-1.0-1" + Distribution.replace("-snapshots", "") + "_all"
+    Build_dir = "repo-%s-%s_all" % (Repo, Version)
 
     if os.path.exists(os.path.join(Build_root, Build_dir)):
         shutil.rmtree(os.path.join(Build_root, Build_dir))
@@ -114,6 +151,7 @@ def Create_repo_deb(Path, Repo, Distribution, Release = False):
     os.makedirs(os.path.join(Build_root, Build_dir, "etc", "apt", "trusted.gpg.d"))
 
     Control_filename = os.path.join(Build_root, Build_dir, "DEBIAN", "control")
+    Script_filename= os.path.join(Build_root, Build_dir, "DEBIAN", "postinst")
     List_filename = os.path.join(Build_root, Build_dir, "etc", "apt", "sources.list.d", Repo + ".list")
     Key_filename = os.path.join(Build_root, Build_dir, "etc", "apt", "trusted.gpg.d", Repo + ".gpg")
 
@@ -121,6 +159,12 @@ def Create_repo_deb(Path, Repo, Distribution, Release = False):
     Destination = open(Control_filename, "w")
     Destination.write(Control_file)
     Destination.close()
+
+    # Add Script file
+    Destination = open(Script_filename, "w")
+    Destination.write(Script_file)
+    Destination.close()
+    os.chmod(Script_filename, 775)
 
     # Add list file
     Destination = open(List_filename, "w")
@@ -135,9 +179,9 @@ def Create_repo_deb(Path, Repo, Distribution, Release = False):
     subprocess.call(Command, stdout=OUT, stderr=OUT)
 
     if not os.path.exists(os.path.join(Build_root, Build_dir) + ".deb"):
-        print("ERROR: activation package failed for distribution %s" % Distribution)
+        print("ERROR: deb activation package failed")
     else:
-        shutil.move(os.path.join(Build_root, Build_dir) + ".deb", os.path.join(Path, Build_dir) + ".deb" )
+        shutil.move(os.path.join(Build_root, Build_dir) + ".deb", os.path.join(Path, "..", Build_dir) + ".deb" )
 
     # Clean
     shutil.rmtree(os.path.join(Build_root, Build_dir))
@@ -166,6 +210,23 @@ def Add_rpm_package(Package, Name, Version, Arch, Distribution, Release = False)
     shutil.copyfile(Package, Package_final)
     Sign_rpm_package(Package_final)
 
+    # Create activation package if needed
+    Repo = Configuration["Repo_name"]
+    if not Release:
+        Repo += "-snapshots"
+
+    Activation_rpm_file = "repo-%s-%s.noarch.rpm" % (Repo, Configuration["Repo_version"])
+
+    if not os.path.isfile(os.path.join(Package_directory, Activation_rpm_file)):
+        # Remove old versions
+        map(os.remove, glob(os.path.join(Package_directory, "repo-%s-*.noarch.rpm" % Repo)))
+
+        if not os.path.isfile(os.path.join(Package_directory, "..", "..", "..", Activation_rpm_file)):
+            Create_repo_rpm(os.path.normpath(os.path.join(Package_directory, "..", "..", "..")), Repo, Release)
+
+        if os.path.isfile(os.path.join(Package_directory, "..", "..", "..", Activation_rpm_file)):
+            shutil.copyfile(os.path.join(Package_directory, "..", "..", "..", Activation_rpm_file), os.path.join(Package_directory, Activation_rpm_file))
+
     # Update repository
     Command = ["createrepo", "--update", os.path.join(Package_directory, "..")]
     subprocess.call(Command, stdout=OUT, stderr=OUT)
@@ -177,26 +238,21 @@ def Add_rpm_package(Package, Name, Version, Arch, Distribution, Release = False)
               ]
     subprocess.call(Command, stdout=OUT, stderr=OUT)
 
-    # Create activation package if needed
-    if fnmatch.fnmatch(Distribution, "CentOS_*") \
-       or fnmatch.fnmatch(Distribution, "RHEL_*") \
-       or fnmatch.fnmatch(Distribution, "Fedora_*"):
-        Repo = Configuration["Repo_name"]
-        if not Release:
-            Repo += "-snapshots"
-
-        Distribution_path = os.path.normpath(os.path.join(Package_directory, "..", ".."))
-        Distribution_file = "repo-%s-1.0.%s.noarch.rpm" % (Repo, Distribution)
-
-        if not os.path.isfile(os.path.join(Distribution_path, Distribution_file)):
-            Create_repo_rpm(Distribution_path, Repo, Distribution, Release)
+    # Export key if needed
+    Key_filename = os.path.join(Package_directory, "..", "repodata", "repomd.xml.key")
+    if not os.path.isfile(Key_filename):
+        Command = ["gpg", "--export", "--armor", "-u", Configuration["Repo_key"]["key"], "--output", Key_filename]
+        subprocess.call(Command, stdout=OUT, stderr=OUT)
 
 #
 # Add_deb_package - add package to his repository, create the repository if don't exist.
 #
 def Add_deb_package(Package, Name, Version, Arch, Distribution, Release = False):
     Dest = "Debian" if fnmatch.fnmatch(Distribution, "Debian_*") else "Ubuntu"
-    Arch = "amd64" if Arch == "x86_64" else "i386"
+    if Arch == "x86_64":
+        Arch = "amd64"
+    elif Arch == "i586":
+        Arch = "i386"
 
     if not Configuration[Dest + "_names"].has_key(Distribution):
         print("ERROR: unable to import package %s, unknown distribution %s" % (Package, Distribution))
@@ -242,14 +298,30 @@ def Add_deb_package(Package, Name, Version, Arch, Distribution, Release = False)
     subprocess.call(Command, stdout=OUT, stderr=OUT)
 
     # Create activation package if needed
-    Repo = Configuration["Repo_name"]
-    if not Release:
-        Repo += "-snapshots"
+    Repo = Configuration["Repo_name"] + "" if Release else Configuration["Repo_name"] + "-snapshots"
 
-    Distribution_file = "repo-%s-1.0-1%s_all.deb" % (Repo, Configuration[Dest + "_names"][Distribution])
+    Activation_deb_file = "repo-%s-%s_all.deb" % (Repo, Configuration["Repo_version"])
 
-    if not os.path.isfile(os.path.join(Cache_directory, Distribution_file)):
-        Create_repo_deb(Cache_directory, Repo, Dist, Release)
+    if not os.path.isfile(os.path.join(Deb_directory, Activation_deb_file)):
+        # Remove old versions
+        map(os.remove, glob(os.path.join(Deb_directory, "repo-%s*_all.deb" % Repo)))
+
+        if not os.path.isfile(os.path.join(Cache_directory, "..", Activation_deb_file)):
+            Create_repo_deb(Cache_directory, Repo, Release)
+
+        if os.path.isfile(os.path.join(Cache_directory, "..", Activation_deb_file)):
+            Command = [ "freight-add", "-c", os.path.join(Cache_directory, "conf", "freight.conf"),
+                        os.path.join(Cache_directory, "..", Activation_deb_file), "apt/" + Dist
+                      ]
+            subprocess.call(Command, stdout=OUT, stderr=OUT)
+
+    # Update repository
+    Command = [ "freight-cache", "-c", os.path.join(Cache_directory, "conf", "freight.conf"),
+                "-g", Configuration["Repo_key"]["key"],
+                "-p", Configuration["Repo_key"]["passfile"],
+                "apt/" + Dist
+              ]
+    subprocess.call(Command, stdout=OUT, stderr=OUT)
 
 #
 # Main
